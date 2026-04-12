c095af9c531071d50d7db6aee3f05940a28b1133
Ivy Tran <ngocthuytien.ivy.tran@gmail.com>
2026-04-12 16:20:38 +1000

    Update #120426: Added supabaseURL debug

--- src/api/SupabaseClient.js
+++ src/api/SupabaseClient.js
@@ -3,6 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+  console.error("Missing Supabase Environment Variables!");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function test() {
