3cbc26e60e0ffe7e3df270e0a064041d6d1f7b5e
Ivy (Thuy Tien) <ngocthuytien.ivy.tran@gmail.com>
2026-06-03 19:42:20 +1000

    Update #030626: fix build error

--- src/api/data/Branch.js
+++ src/api/data/Branch.js
@@ -1,49 +1,55 @@
-import {supabase} from '../supabaseClient';
+import { supabase } from "../supabaseClient.js";
 
 export async function getBranchList(searchKeyword) {
   try {
-    const query = supabase.from("branch").select().order("name", { ascending: true });
+    const query = supabase
+      .from("branch")
+      .select()
+      .order("name", { ascending: true });
     if (searchKeyword && searchKeyword.trim() !== "") {
       query.ilike("name", `%${searchKeyword}%`);
     }
     const { data, error } = await query;
-    if (error) return {data: null, status: "error", message: error.message};
-    return {data, status: "success"};
+    if (error) return { data: null, status: "error", message: error.message };
+    return { data, status: "success" };
   } catch (error) {
     console.error("Error fetching branch list:", error);
-    return {data: null, status: "error", message: error.message};
+    return { data: null, status: "error", message: error.message };
   }
 }
 
 export async function addBranch(branchData) {
   try {
     const { error } = await supabase.from("branch").insert(branchData);
-    if (error) return {status: "error", message: error.message};
-    return {status: "success"};
+    if (error) return { status: "error", message: error.message };
+    return { status: "success" };
   } catch (error) {
     console.error("Error adding branch:", error);
-    return {status: "error", message: error.message};
+    return { status: "error", message: error.message };
   }
 }
 
 export async function updateBranch(branchData) {
   try {
-    const { error } = await supabase.from("branch").update(branchData).eq("id", branchData.id);
-    if (error) return {status: "error", message: error.message};
-    return {status: "success"};
+    const { error } = await supabase
+      .from("branch")
+      .update(branchData)
+      .eq("id", branchData.id);
+    if (error) return { status: "error", message: error.message };
+    return { status: "success" };
   } catch (error) {
     console.error("Error updating branch:", error);
-    return {status: "error", message: error.message};
+    return { status: "error", message: error.message };
   }
 }
 
 export async function deleteBranch(branchId) {
   try {
     const { error } = await supabase.from("branch").delete().eq("id", branchId);
-    if (error) return {status: "error", message: error.message};
-    return {status: "success"};
+    if (error) return { status: "error", message: error.message };
+    return { status: "success" };
   } catch (error) {
     console.error("Error deleting branch:", error);
-    return {status: "error", message: error.message};
-  } 
+    return { status: "error", message: error.message };
+  }
 }
